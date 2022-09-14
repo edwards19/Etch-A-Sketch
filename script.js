1f38ff51196e1e7eb46426a5ccd23e792dca0a0c
Eduard <eduard.cojocaru26@gmail.com>
2022-09-14 20:13:03 +0300

    Refactor the game logic and styles

--- script.js
+++ script.js
@@ -1,57 +1,50 @@
 const container = document.querySelector('.container');
-const div = document.createElement('div');
-container.setAttribute('style', 'display: grid; grid-template-columns: repeat(16, 30px); gap: 2px;');
+const pixelSize = document.querySelector('.size')
+let size = pixelSize.value;
+const color = document.querySelector('.color');
+const reset = document.querySelector('.reset');
 
-const inputs = [...document.querySelectorAll('input')];
-inputs.forEach((input) => {
-  input.addEventListener('click', (e) => {
-    //console.log(input.value ** 2)
-    appendElements(e.target.value ** 2)
-  }, { once: true })
-})
-  
-// const first = document.getElementById('first');
-// first.addEventListener('click', (e) => {
-//   appendElements((first.value ** 2))
-//   window.reset()
-//   //window.location.reload()
-// });
+let draw = false;
 
-// const second = document.getElementById('second');
-// second.addEventListener('click', (e) => {
-//   appendElements(second.value ** 2)
-// })
 
-// const second = document.getElementById('second');
-// second.addEventListener('click', (e) => {
-//   appendElements(second.value ** 2)
-// })
-
-// const reset = document.createElement('button');
-// reset.style.display = 'block';
-// const resetText = document.createTextNode('Reset');
-// reset.appendChild(resetText);
-// container.appendChild(reset)
-
-function appendElements(number = 16) {
-  for (let i = 0; i < number; i++){
+function appendPixels(size) {
+  for (let i = 0; i < size ** 2; i++){
+    // This line takes the css variable --size and replaces it with the function parameter from above
+    container.style.setProperty('--size', size);
     const div = document.createElement('div');
-    div.setAttribute('style', 'width:30px; height:30px; outline:2px solid black; background-color:lightblue;');
-    div.addEventListener('mouseover', () => {
-      div.style.backgroundColor = 'lightpink';
-    })
-    // container.addEventListener('click', () => {
-    //   div.style.backgroundColor = 'lightblue';
-    // })
+    div.classList.toggle('pixel');
     container.appendChild(div);
-    const reset = document.querySelector('.reset');
-    reset.addEventListener('click', () => {
-      div.style.backgroundColor = 'lightblue';
+    div.addEventListener('mouseover', () => {
+      if(!draw) return;
+      div.style.backgroundColor = color.value;
+    })
+    div.addEventListener('mousedown', () => {
+      div.style.backgroundColor = color.value;
     })
   }
-}
+};
 
-appendElements(16 ** 2)
+window.addEventListener('mousedown', () => {
+  draw = true;
+});
+
+window.addEventListener('mouseup', () => {
+  draw = false;
+});
+
+function restart() {
+  container.textContent = '';
+  appendPixels(size);
+};
+
+reset.addEventListener('click', restart);
+
+pixelSize.addEventListener('keyup', () => {
+  size = pixelSize.value;
+  restart()
+})
+
+appendPixels(size);
 
 
 // const cells = 529;
