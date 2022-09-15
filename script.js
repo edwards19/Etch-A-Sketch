27d2967bebcc54a17f8b00b87c5af806d48dd35a
Eduard <eduard.cojocaru26@gmail.com>
2022-09-15 13:32:17 +0300

    Add an eraser button and a footer

--- script.js
+++ script.js
@@ -3,25 +3,51 @@ const pixelSize = document.querySelector('.size')
 let size = pixelSize.value;
 const color = document.querySelector('.color');
 const reset = document.querySelector('.reset');
+const eraser = document.querySelector('.eraser');
 
 let draw = false;
 
 
 function appendPixels(size) {
-  for (let i = 0; i < size ** 2; i++){
-    // This line takes the css variable --size and replaces it with the function parameter from above
-    container.style.setProperty('--size', size);
-    const div = document.createElement('div');
-    div.classList.toggle('pixel');
-    container.appendChild(div);
-    div.addEventListener('mouseover', () => {
-      if(!draw) return;
-      div.style.backgroundColor = color.value;
-    })
-    div.addEventListener('mousedown', () => {
-      div.style.backgroundColor = color.value;
-    })
+  if (size <= 80) {
+    for (let i = 0; i < size ** 2; i++) {
+      // This line takes the css variable --size and replaces it with the function parameter from above
+      container.style.setProperty('--size', size);
+      const div = document.createElement('div');
+      div.classList.toggle('pixel');
+      container.appendChild(div);
+      div.addEventListener('mouseover', () => {
+        if (!draw) return;
+        div.style.backgroundColor = color.value;
+      })
+      div.addEventListener('mousedown', () => {
+        div.style.backgroundColor = color.value;
+      })
+      eraser.addEventListener('click', () => {
+        div.addEventListener('mouseover', () => {
+          if (!draw) return;
+          div.style.backgroundColor = 'beige'
+        })
+        div.addEventListener('mousedown', () => {
+          div.style.backgroundColor = 'beige'
+        })
+      })
+      color.addEventListener('click', () => {
+        div.addEventListener('mouseover', () => {
+          if (!draw) return;
+          div.style.backgroundColor = color.value;
+        })
+        div.addEventListener('mousedown', () => {
+          div.style.backgroundColor = color.value;
+        })
+      })
+     
+    }
+  } else {
+    alert('The max size for the grid is 100, please introduce a new value!');
+    appendPixels(30);
   }
+  
 };
 
 window.addEventListener('mousedown', () => {
@@ -42,19 +68,8 @@ reset.addEventListener('click', restart);
 pixelSize.addEventListener('keyup', () => {
   size = pixelSize.value;
   restart()
-})
+});
 
 appendPixels(size);
 
 
-// const cells = 529;
-
-// const columns = Math.sqrt(cells);
-
-// const result = appendElements(cells);
-
-// // const cells = Math.sqrt(`${result}`);
-
-// // console.log(cells)
-
-// console.log(columns)
